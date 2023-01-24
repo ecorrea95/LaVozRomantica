ba871d39aa40e0d273e07d907ce749a78da0a973
Emiliano Correa <e.correa95@hotmail.com>
2023-01-23 22:01:28 -0300

    Agregados componentes home, carrera, galeria, contacto, footer

--- LaVozRomantica/src/app/app.module.ts
+++ LaVozRomantica/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CarreraComponent } from './carrera/carrera.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CarreraComponent,
+    GaleriaComponent,
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
