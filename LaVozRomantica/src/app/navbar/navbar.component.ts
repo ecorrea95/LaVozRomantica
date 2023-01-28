dbd996d68bbbc5be10b4ad547574e0477ac4d776
Emiliano Correa <e.correa95@hotmail.com>
2023-01-28 11:38:24 -0300

    Agregada animacion de navbar

--- LaVozRomantica/src/app/navbar/navbar.component.ts
+++ LaVozRomantica/src/app/navbar/navbar.component.ts
@@ -1,9 +1,24 @@
 import { Component, HostListener } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  animations: [
+    trigger('enterNavbar', [
+      state('void', style({
+        transform: 'translateY(100%)',
+        opacity: 0
+      })),
+      transition(':enter', [
+        animate(300, style({
+          transform: 'translateY(0)',
+          opacity: 1
+        }))
+      ])
+    ])
+  ]
 })
 export class NavbarComponent {
 
