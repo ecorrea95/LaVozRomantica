dca93a6d5aa077637b941267075afec39fc4018b
Emiliano Correa <e.correa95@hotmail.com>
2023-01-28 11:57:18 -0300

    Agregada primer animación de home

--- LaVozRomantica/src/app/home/home.component.ts
+++ LaVozRomantica/src/app/home/home.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { trigger, style, transition, animate, state } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('enterTitle', [
+      state('void', style({
+        transform: 'translateX(-100%)',
+        // opacity: 0
+      })),
+      transition(':enter', [
+        animate(1000, style({
+          transform: 'translateX(0)',
+          // opacity: 1
+        }))
+      ])
+    ]),
+    trigger('enterMasthead', [
+      state('void', style({
+        backdropFilter: 'brightness(0%)',
+        opacity: 0
+      })),
+      transition(':enter', [
+        animate(300, style({
+          backdropFilter: 'brightness(100%)',
+          opacity: 1
+        }))
+      ])
+    ]),
+
+  ]
 })
 export class HomeComponent {
 
